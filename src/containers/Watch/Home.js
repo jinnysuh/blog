5be6d1e6a33094a6eb223d0c2422a6b33d7fd30d
jinnysuh <jinnysuh2006@gmail.com>
2017-11-11 14:32:21 +0900

    i changed the video

--- src/containers/Watch/Home.js
+++ src/containers/Watch/Home.js
@@ -7,12 +7,12 @@ export default function Home() {
       <h1>Fun Minecraft Videos!</h1>
       <Video videoCode="0JnzM8E88KA"/>
        <h2>*NEW!*</h2>
-       <Video videoCode="x6Ah7qBgnKQ"/>
-      <Video videoCode="fGl5T6AhvWs"/>
+       <Video videoCode="fGl5T6AhvWs"/>
       <Video videoCode="5upz6oAMlm4"/>
        <Video videoCode= "g5MXupG7kPI"/>
       <Video videoCode="_zZaHaHFd5E"/>
       <Video videoCode="fjXs5wNpgZQ"/>
+      <Video videoCode="x6Ah7qBgnKQ"/>
       <Video videoCode="976wSavslj0"/>
       <Video videoCode="1nHZm4Elbm8"/>
       <Video videoCode="jYQhUaWbh3E"/>
