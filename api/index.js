fa6278fe5d1019c7838025cfa69dec3ecdbb7f45
mikey1384 <mikey1384@gmail.com>
2019-04-26 18:56:47 +0800

    add https to api server

--- api/index.js
+++ api/index.js
@@ -1,7 +1,36 @@
+require('dotenv').config();
 const app = require('./app');
-const http = require('http').Server(app);
-const PORT = 3500;
+const https = require('https');
+const http = require('http');
 
-http.listen(PORT, () => {
-  console.log('Server listening on api port:', PORT);
+const lex = require('greenlock-express').create({
+  version: 'draft-12',
+  server: 'https://acme-v02.api.letsencrypt.org/directory',
+  email: 'jinnysuh2006@gmail.com',
+  agreeTos: true,
+  approveDomains: ['api.jinny2006.com'],
+  challenges: {
+    'http-01': require('le-challenge-fs').create({
+      webrootPath: '/tmp/acme-challenges'
+    })
+  }
 });
+
+const PORT = process.env.PORT || 3500;
+
+const server =
+  process.env.NODE_ENV === 'production'
+    ? https.createServer(lex.httpsOptions, lex.middleware(app))
+    : http.Server(app);
+
+server.listen(PORT, function() {
+  console.log('Server listening on port:', PORT);
+});
+
+if (process.env.NODE_ENV === 'production') {
+  require('http')
+    .createServer(lex.middleware(require('redirect-https')()))
+    .listen(80, function() {
+      console.log('Server listening on port:', 80);
+    });
+}
